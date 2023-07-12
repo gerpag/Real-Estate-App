5f4827b84634b3f916702b6e511ce7e7557ac612
VincentMax90 <maxclc@hotmail.com>
2023-07-12 12:23:20 -0300

    view

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router";
+
+import Home from "./views/Home";
+import Navbar from "./components/Navbar";
+import Login from "./views/Login";
+import Register from "./views/Register";
+
 
 function App() {
-  return <div className="App">HOLA</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ front/src/views/Login.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+
+function Login() {
+  return (
+    <>
+    <Box
+        sx={{display:"flex",
+        flexWrap:"wa", 
+          width: "100vh",
+          height: "100vh",
+          backgroundColor: "red",
+        }}
+      /> 
+      <img src="https://casasinhaus.com/wp-content/uploads/2021/10/casas-prefabricadas-valencia-fachada-1320x853.jpg" width= {"300px"}
+          height= {"300px"}></img>/    </>
+  );
+}
+export default Login;
