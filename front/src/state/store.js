b841776ec6a02f086f58b7dfe2a2916c15396779
gerpag <germinalfp@gmail.com>
2023-07-15 00:13:38 -0300

    en esta rama modificamos el estado de redux y los componentes login y navbar para manejar persistencia desde back

--- front/src/state/store.js
+++ front/src/state/store.js
@@ -1,23 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
 import reducerUser from "./user";
-
-
-const persistenceState = localStorage.getItem("reduxState")
-  ? JSON.parse(localStorage.getItem("reduxState"))
-  : {};
 
 const store = configureStore({
   reducer: {
     user: reducerUser,
-    
   },
-  preloadedState: persistenceState,
-});
-
-store.subscribe(() => {
-  const state = store.getState();
-  localStorage.setItem("reduxState", JSON.stringify(state));
 });
 
 export default store;
